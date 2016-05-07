041ee84e58830c41acdfb1c698e2dcb56cabbb91
elifseyhan <elfsyhan@hotmail.com>
2016-05-07 16:46:49 +0300

    add post_list.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,4 @@
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -7,4 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
